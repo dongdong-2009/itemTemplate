5052ae8c284555d54842e63e7ef93d8e853d7480
ixueping <ixueping@126.com>
2014-08-31 10:42:28 +0800

    test uid

---
+++ src/junit/TestUUID.java
@@ -0,0 +1,19 @@
+package junit;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class TestUUID {
+	public static void main(String[] args) {
+		UUID uuid = UUID.randomUUID();
+		String u = uuid.toString().replace("-", "").toUpperCase();
+		System.out.println("UUID:"+u);
+		
+		String qqNumber = "68547302";
+		String sep = "|";
+		//格式化日期 yyyyMMdd
+		String date = String.format("%tY%<tm%<td", new Date());
+		System.out.println("---------------");
+		System.out.println("订单号:"+qqNumber+sep+date+sep);
+	}
+}
